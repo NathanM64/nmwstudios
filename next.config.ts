855c2ed054b468dee0c5025a551012dfbc43c18e
Nathan Marimbordes <marimbordes.nathan@gmail.com>
2025-10-26 22:01:52 +0100

    fix: redirection auto www -> non www

--- next.config.ts
+++ next.config.ts
@@ -4,22 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-  async redirects() {
-    return [
-      // Redirection www vers non-www
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.nmwstudios.com',
-          },
-        ],
-        destination: 'https://nmwstudios.com/:path*',
-        permanent: true, // 301 redirect
-      },
-    ]
-  },
 };
 
 export default nextConfig;
